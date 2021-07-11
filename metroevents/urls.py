5ad08dd0f740cc137a4a5008bc6f5e72f232814e
neff-1 <47888850+JukKie-ai@users.noreply.github.com>
2021-07-12 00:19:59 +0800

    added pages

--- metroevents/urls.py
+++ metroevents/urls.py
@@ -13,9 +13,18 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls.static import static
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
+    path('metroevents/', include('user.urls', namespace='user'))
+
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- user/views.py
+++ user/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from django.views.generic.base import View
+from .models import *
+from .forms import *
 
 # Create your views here.
+class dashboardView(View):
+    template_name = "user/dashboard.html"
+
+    def get(self, request):
+        return render(request, self.template_name)
+
+class loginView(View):
+    template_name = "user/login.html"
+
+    def get(self, request):
+        return render(request, self.template_name)
+
+class registerView(View):
+    template_name = "user/register.html"
+
+    def get(self, request):
+        return render(request, self.template_name)
