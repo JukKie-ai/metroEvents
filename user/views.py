9a050bec60b44efbfb9be6faeeb002de2e2161dd
La2r <jkofianga@gmail.com>
2021-08-01 19:33:53 +0800

    Changes
    joinList
    createEvent
    organizedEvents
    updateEvent

--- user/views.py
+++ user/views.py
@@ -97,13 +97,19 @@ class joinListView(View):
         if Organizer.objects.filter(username=user).count() != 0:
             account = Organizer.objects.get(username=user)
             obj = User.objects.all()
-            obj2 = Event.objects.all()
-        else:
-            account = 0
-            obj = '0'
-            obj2 = '0'
+            
+            if Organizer.objects.filter(username=user).count() != 0:
+                account = Organizer.objects.get(username=user)
+                obj2 = Event.objects.all() 
+            else:
+                account = 0
+                obj = 0
+                obj2 = '0'
 
-        return render(request, self.template_name, {'obj':obj, 'account': account})
+        return render(request, self.template_name, {'obj':obj, 'obj2':obj2,'account': account})
+
+    def post(self, request, user):
+        status = RequestEvent.objects.get()
 
 
 def updateEventView(request, id, user):
